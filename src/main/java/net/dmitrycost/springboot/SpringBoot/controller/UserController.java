9bdf25bedccff006918e2aee1916b8492d75cc94
DmitryCost <70369805+DmitryCost@users.noreply.github.com>
2023-03-06 14:23:33 +0700

    fix

--- src/main/java/net/dmitrycost/springboot/SpringBoot/controller/UserController.java
+++ src/main/java/net/dmitrycost/springboot/SpringBoot/controller/UserController.java
@@ -2,27 +2,25 @@ package net.dmitrycost.springboot.SpringBoot.controller;
 
 import jakarta.validation.Valid;
 import net.dmitrycost.springboot.SpringBoot.model.User;
-import net.dmitrycost.springboot.SpringBoot.service.UserService;
+import net.dmitrycost.springboot.SpringBoot.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Controller
 public class UserController {
-    private final UserService userService;
+    private final UserServiceImpl userServiceImpl;
 
     @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
+    public UserController(UserServiceImpl userServiceImpl) {
+        this.userServiceImpl = userServiceImpl;
     }
 
     @GetMapping(value = "/")
     public String home(ModelMap model) {
-        model.addAttribute("users", userService.listAll());
+        model.addAttribute("users", userServiceImpl.listAll());
         return "index";
     }
 
@@ -37,19 +35,19 @@ public class UserController {
         if(bindingResult.hasErrors()) {
             return "new_user";
         }
-        userService.save(user);
+        userServiceImpl.save(user);
         return "redirect:/";
     }
 
     @GetMapping("/{id}/edit")
     public String edit(ModelMap model, @PathVariable("id") Long id) {
-        model.addAttribute("user", userService.getById(id));
+        model.addAttribute("user", userServiceImpl.getById(id));
         return "edit_user";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") Long id, ModelMap model) {
-        model.addAttribute("user", userService.getById(id));
+        model.addAttribute("user", userServiceImpl.getById(id));
         return "show_user";
     }
 
@@ -60,13 +58,13 @@ public class UserController {
         if (bindingResult.hasErrors()) {
             return "edit_user";
         }
-        userService.save(user);
+        userServiceImpl.save(user);
         return "redirect:/";
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") Long id) {
-        userService.deleteById(id);
+        userServiceImpl.deleteById(id);
         return "redirect:/";
     }
 
--- src/main/java/net/dmitrycost/springboot/SpringBoot/service/UserService.java
+++ src/main/java/net/dmitrycost/springboot/SpringBoot/service/UserService.java
@@ -1,34 +1,13 @@
 package net.dmitrycost.springboot.SpringBoot.service;
 
 import net.dmitrycost.springboot.SpringBoot.model.User;
-import net.dmitrycost.springboot.SpringBoot.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class UserService {
-    private final UserRepository userRepository;
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-    public void save(User user) {
-        userRepository.save(user);
-    }
+public interface UserService {
+    void save(User user);
+    List<User> listAll();
+    User getById(Long id);
+    void deleteById(Long id);
 
-    public List<User> listAll() {
-        return userRepository.findAll();
-    }
-
-
-    public User getById(Long id) {
-        return userRepository.findById(id).orElse(null);
-    }
-
-
-    public void deleteById(Long id) {
-        userRepository.deleteById(id);
-    }
 }
